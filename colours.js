b8f6baf49b1fc1aac5b222edd038380ff0f7ff68
Kareem Saleh <kl.kasaleh98@gmail.com>
2025-07-17 20:58:52 +0100

    First version

---
+++ colours.js
@@ -0,0 +1,72 @@
+colours = {
+  "Cosmic Production": {
+    "Eichi Tenshouin": {
+      colour: "#FFE58E",
+      img: "imgs/characters/starmakerEichi_Tenshouin_4.webp",
+    },
+    "Wataru Hibiki": {
+      colour: "#AEE0DF",
+      img: "imgs/characters/starmaker/Wataru_Hibiki_4.webp",
+    },
+    "Tori Himemiya": {
+      colour: "#FCD9DF",
+      img: "imgs/characters/starmaker/Tori_Himemiya_4.webp",
+    },
+    "Yuzuru Fushimi": {
+      colour: "#A99EFC",
+      img: "imgs/characters/starmaker/Yuzuru_Fushimi_4.webp",
+    },
+    "Hokuto Hidaka": {
+      colour: "#0F3190",
+      img: "imgs/characters/trickstar/Hokuto_Hidaka_4.webp",
+    },
+    "Subaru Akehoshi": {
+      colour: "#FC2E3B",
+      img: "imgs/characters/trickstar/Subaru_Akehoshi_4.webp",
+    },
+    "Makoto Yuuki": {
+      colour: "#FFB348",
+      img: "imgs/characters/trickstar/Makoto_Yuuki_4.webp",
+    },
+    "Mao Isara": {
+      colour: "#BB1754",
+      img: "imgs/characters/trickstar/Mao_Isara_4.webp",
+    },
+    "Tetora Nagumo": {
+      colour: "#362A2C",
+      img: "imgs/characters/ryuseutai/Tetora_Nagumo_5.webp",
+    },
+    "Midori Takamine": {
+      colour: "#51AC23",
+      img: "imgs/characters/ryuseutai/Midori_Takamine_5.webp",
+    },
+    "Shinobu Sengoku": {
+      colour: "#FFFF05",
+      img: "imgs/characters/ryuseutai/Shinobu_Sengoku_5.webp",
+    },
+    "Chiaki Morisawa": {
+      colour: "#FF0E03",
+      img: "imgs/characters/ryuseutai/Chiaki_Morisawa_5.webp",
+    },
+    "Kanata Shinkai": {
+      colour: "#5EC2DA",
+      img: "imgs/characters/ryuseutai/Kanata_Shinkai_5.webp",
+    },
+    "Hiiro Amagi": {
+      colour: "#BA2636",
+      img: "imgs/characters/alkaloid/Hiiro_Amagi_4.webp",
+    },
+    "Aira Shiratori": {
+      colour: "#FFF0CF",
+      img: "imgs/characters/alkaloid/Aira_Shiratori_4.webp",
+    },
+    "Mayoi Ayase": {
+      colour: "#522F5F",
+      img: "imgs/characters/alkaloid/Mayoi_Ayase_4.webp",
+    },
+    "Tatsumi Kazehaya": {
+      colour: "#7EBDA6",
+      img: "imgs/characters/alkaloid/Tatsumi_Kazehaya_4.webp",
+    },
+  },
+};
---
+++ select_agency.js
@@ -0,0 +1,60 @@
+const selected = new Set();
+let selectedText = "You have selected";
+const agencySelected = document.querySelector(".agency-selected");
+const playBtn = document.querySelector(".play-btn");
+let playable = false;
+
+function checkSelectedEmpty(selected) {
+  if (selected.size !== 0) {
+    playBtn.style.backgroundColor = "rgb(59, 246, 59)";
+    playBtn.style.cursor = "pointer";
+    playable = true;
+  } else {
+    playBtn.style.backgroundColor = "rgba(182, 182, 182, 1)";
+    playBtn.style.cursor = "auto";
+    playable = false;
+  }
+}
+
+checkSelectedEmpty(selected);
+
+document.addEventListener("click", (event) => {
+  const clicked = event.target.closest(".agency");
+  const id = clicked.id;
+
+  if (clicked) {
+    clicked.classList.toggle("selected");
+
+    if (selected.has(id)) {
+      selected.delete(id);
+    } else {
+      selected.add(id);
+    }
+
+    const selectedArray = Array.from(selected);
+
+    if (selectedArray.length === 0) {
+      selectedText = "Select the group / groups you want to play!";
+      agencySelected.innerText = selectedText;
+    }
+
+    for (let i = 0; i < selectedArray.length; i++) {
+      let name = selectedArray[i];
+
+      if (selectedArray.length === 1) {
+        selectedText += ` ${name}.`;
+        break;
+      }
+      if (i === selectedArray.length - 1) {
+        selectedText += ` and ${name}.`;
+        break;
+      } else if (i >= 0) {
+        selectedText += ` ${name},`;
+      }
+    }
+  }
+
+  checkSelectedEmpty(selected);
+  agencySelected.innerText = selectedText;
+  selectedText = "You have selected";
+});
